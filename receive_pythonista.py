17471019e4b4b863248f943a9d45dcf4fd5d49ea
lxu <lxu@junzhiam.com>
2021-08-02 13:31:30 +0800

    fix imports in brython

--- receive_pythonista.py
+++ receive_pythonista.py
@@ -1,10 +1,97 @@
+import base64
+import hashlib
 from ctypes import c_void_p
+from functools import cached_property
+from pathlib import Path
+from typing import Dict, Optional, Set
 
 import sound
 import ui
 from objc_util import *
 
-from data import Decoder
+
+class Decoder:
+    def __init__(self):
+        self.expected_iterations: Optional[Set[bytes]] = None
+        self.received_iterations: Dict[bytes, bytes] = {}
+        self.file_name = None
+        self.hash = None
+        self.length = None
+
+    def set_name(self, name: str):
+        print(f'[*] File name: {name}')
+        self.file_name = name
+
+    def set_length(self, length: int):
+        print(f'[*] The message will come in {length} parts')
+        self.length = length
+        self.expected_iterations = {b'NAME', b'LEN', b'HASH'} | {
+            str(i + 1).encode() for i in range(length)
+        }
+
+    def set_hash(self, hash: str):
+        print(f'[*] Hash {hash}')
+        self.hash = hash
+
+    @property
+    def expecting(self) -> Set[bytes]:
+        assert self.expected_iterations is not None
+        return self.expected_iterations - set(self.received_iterations.keys())
+
+    @property
+    def is_finished(self) -> bool:
+        if self.length is None:
+            return False
+        return len(self.expecting) == 0
+
+    @cached_property
+    def data(self):
+        assert self.is_finished
+        data = b''.join(
+            [
+                base64.b64decode(data)
+                for i, data in sorted(
+                    self.received_iterations.items(),
+                    key=lambda _: int(_[0])
+                    if _[0] not in {b'NAME', b'LEN', b'HASH'}
+                    else 0,
+                )
+                if i not in {b'NAME', b'LEN', b'HASH'}
+            ]
+        )
+        return data
+
+    def check_integrity(self):
+        assert self.is_finished
+
+        final_hash = hashlib.sha1(self.data).hexdigest()
+
+        if final_hash != self.hash:
+            raise ValueError(f'[*] Expected: {self.hash}, got: {final_hash}')
+
+    def save_file(self, folder: Path = Path('.')):
+        file = folder / self.file_name
+        file.write_bytes(self.data)
+        print(f'File saved to {file}')
+
+    def process_chunk(self, chunk: bytes):
+        i, data = chunk.split(b':', maxsplit=1)
+        if i in self.received_iterations:
+            return False
+
+        self.received_iterations[i] = data
+
+        if i == b'NAME':
+            self.set_name(data.decode())
+
+        if i == b'LEN':
+            self.set_length(int(data))
+
+        if i == b'HASH':
+            self.set_hash(data.decode())
+
+        return True
+
 
 decoder = Decoder()
 main_view = None
