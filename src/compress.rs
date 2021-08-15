38459d5e05e1cbca43afe1cf2d081521a2eb344b
WestXu <xu-lai-xi@qq.com>
2021-08-15 16:28:18 +0800

    compress: fix warning using write_all

--- src/compress.rs
+++ src/compress.rs
@@ -4,7 +4,7 @@ pub fn compress(input: Vec<u8>) -> Vec<u8> {
     let mut output = Vec::new();
     {
         let mut writer = brotli::CompressorWriter::new(&mut output, 4096, 11, 22);
-        writer.write(&input);
+        writer.write_all(&input).expect("Failed compressing.");
     }
     output
 }
@@ -13,7 +13,7 @@ pub fn decompress(input: Vec<u8>) -> Vec<u8> {
     let mut output = Vec::new();
     {
         let mut writer = brotli::DecompressorWriter::new(&mut output, 4096);
-        writer.write(&input);
+        writer.write_all(&input).expect("Failed decompressing.");
     }
     output
 }
