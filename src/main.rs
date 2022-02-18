b8dde2e7567ccf3d46b8d285fef1e54cf9d67852
lxu <lxu@junzhiam.com>
2022-02-18 14:52:02 +0800

    move index.html to dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dioxus::prelude::*;
 use js_sys::Reflect;
 use qrtransfer::utils::{log, set_panic_hook};
 use qrtransfer::{compress, decoder, encoder};
@@ -31,6 +32,136 @@ impl QrTransfer {
     }
 }
 
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            id: "outer-div",
+            div {
+                class: "form-check form-switch float",
+                id: "scroll-check-div",
+                style: "display: none;",
+                input {
+                    class: "form-check-input",
+                    id: "scroll-check",
+                    "onclick": "toggle_scroll()",
+                    r#type: "checkbox",
+                }
+                label {
+                    class: "form-check-label",
+                    r#for: "scroll-check",
+                    "Scroll"
+                }
+            }
+            div {
+                id: "middle-div",
+                div {
+                    id: "inner-div",
+                    div {
+                        id: "main-page",
+                        h1 {
+                            "qrtransfer"
+                        }
+                        ul {
+                            class: "nav nav-tabs",
+                            id: "myTab",
+                            role: "tablist",
+                            li {
+                                class: "nav-item",
+                                role: "presentation",
+                                button {
+                                    class: "nav-link active",
+                                    id: "send-tab",
+                                    "aria-controls": "send",
+                                    "aria-selected": "true",
+                                    "data-bs-target": "#send",
+                                    "data-bs-toggle": "tab",
+                                    role: "tab",
+                                    r#type: "button",
+                                    "Send"
+                                }
+                            }
+                            li {
+                                class: "nav-item",
+                                role: "presentation",
+                                button {
+                                    class: "nav-link",
+                                    id: "receive-tab",
+                                    "aria-controls": "receive",
+                                    "aria-selected": "false",
+                                    "data-bs-target": "#receive",
+                                    "data-bs-toggle": "tab",
+                                    role: "tab",
+                                    r#type: "button",
+                                    "Receive"
+                                }
+                            }
+                        }
+                        div {
+                            class: "tab-content",
+                            div {
+                                class: "tab-pane active",
+                                id: "send",
+                                "aria-labelledby": "send-tab",
+                                role: "tabpanel",
+                                input {
+                                    class: "form-control form-control-lg",
+                                    id: "file-selector",
+                                    "onchange": "read_file_content()",
+                                    r#type: "file",
+                                }
+                                div {
+                                    id: "progress",
+                                }
+                                div {
+                                    id: "qrcode",
+                                }
+                            }
+                            div {
+                                class: "tab-pane",
+                                id: "receive",
+                                "aria-labelledby": "receiv-tab",
+                                role: "tabpanel",
+                                video {
+                                    id: "scan-video",
+                                    playsinline: "true",
+                                    autoplay: "true",
+                                }
+                                canvas {
+                                    id: "canvas",
+                                    style: "display: none;",
+                                }
+                                div {
+                                    id: "cam-qr-result",
+                                    style: "white-space: pre;word-wrap:break-word;",
+                                }
+                                br {
+                                }
+                                button {
+                                    class: "btn btn-outline-primary",
+                                    id: "start-button",
+                                    "onclick": "start_receiving()",
+                                    "Start"
+                                }
+                                button {
+                                    class: "btn btn-outline-danger",
+                                    id: "stop-button",
+                                    "onclick": "stop_receiving()",
+                                    "Stop"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        script {
+            crossorigin: "anonymous",
+            src: "https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js",
+            ""
+        }
+    })
+}
+
 fn main() {
     log("Initializing wasm...");
     set_panic_hook();
@@ -42,17 +173,7 @@ fn main() {
     )
     .unwrap();
 
-    let document = window.document().expect("should have a document on window");
-    document
-        .get_element_by_id("spinner")
-        .unwrap()
-        .set_attribute("style", "display: none;")
-        .unwrap();
-    document
-        .get_element_by_id("main-page")
-        .unwrap()
-        .set_attribute("style", "display: block;")
-        .unwrap();
-
     log("Wasm initialized.");
+
+    dioxus::web::launch(app);
 }
