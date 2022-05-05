5cf0ca6e49bfc073105d52413c0bae129f70e6f5
WestXu <WestXu@users.noreply.github.com>
2022-05-05 14:27:44 +0000

    deploy: dec2f50e631919180217ed187771aba36e219713

--- qrtransfer-95cf0dd6188ee63f.js
+++ qrtransfer-95cf0dd6188ee63f.js
@@ -430,7 +430,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('qrtransfer-c8eb68a02b993a65_bg.wasm', import.meta.url);
+        input = new URL('qrtransfer-95cf0dd6188ee63f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
